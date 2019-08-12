32e6a08aa3e78b345a56d320075d072b023c3101
lhsqjoe <lhsqjoe@126.com>
2019-08-12 10:14:30 +0800

    资料整理

---
+++ pythoncookbook/chapter001/001.py
@@ -0,0 +1,23 @@
+p = (4, 5)
+x, y = p
+print(x, "--------", y)
+data = ['ACME', 50, 91.1, (2012, 12, 21)]
+name, shares, price, date = data
+print(name)
+
+print(shares)
+print(price)
+
+print(date)
+
+name, shares, price, (year, mon, day) = data
+
+print(name)
+
+print(shares)
+print(price)
+
+print(year)
+
+print(mon)
+print(day)
